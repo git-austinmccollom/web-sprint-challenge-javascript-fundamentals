e77a4dd6368a6f9f81e22b4a76dd99a7a8e077ac
Austin McCollom <austinmccollom@Austins-Air.attlocal.net>
2020-07-24 11:12:56 -0500

    closures

--- challenges/closure.js
+++ challenges/closure.js
@@ -1,6 +1,7 @@
 // ==== Closures ==== 
 
-/* Task 1: Study the code below and explain in your own words why nested function can access the variable internal. */
+/* Task 1: Study the code below and explain in your own words why nested function can access the 
+variable internal. */
 
 
 const external = "I'm outside the function";
@@ -17,8 +18,23 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+/*
+The nestedFunction can access the "internal" because the variable is in the nestedFunction's parent 
+function and outer scope. Another way of saying it is that "internal" is in nestedFunction's closure.
+*/
 
 /* Task 2: Counter */
 
-/* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+/* Create a function called `summation` that accepts a parameter and uses a counter to return the
+ summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+const summation = function(positiveInteger) {
+  let accumulator = 0;
+  for ( let i = 0; i <= positiveInteger; i++ ) {
+    accumulator += i;
+  }
+  return accumulator;
+}
+
+console.log(summation(4));
+console.log(summation(6));
