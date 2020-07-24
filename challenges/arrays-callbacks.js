780bd4477137d11bd8d3131e66bca145823245bf
Austin McCollom <austinmccollom@Austins-Air.attlocal.net>
2020-07-24 10:53:42 -0500

    arrays-callbacks request 4

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -53,11 +53,15 @@ console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
 
-The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
+The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the 
+.reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for 
+the count.
 
 */
-// let populationTotal = 0;
-// console.log(populationTotal);
+let populationTotal = zooAnimals.reduce(function(accumulator, animal){
+  return accumulator += animal.population;
+}, 0);
+console.log(populationTotal);
 
 
 // ==== Callbacks ====  
